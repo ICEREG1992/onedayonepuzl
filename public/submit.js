526b17288157d4ccc5395116ac0940703cd6f6f0
icereg1992 <willwill.sullivan@gmail.com>
2022-05-08 12:05:23 -0500

    Fix submit form

--- public/submit.js
+++ public/submit.js
@@ -15,30 +15,45 @@ var database = firebase.database();
 
 function postPuzzle(elem) {
 	week_num = document.getElementById('create-week').value;
-	return database.ref("weeks").orderByKey().equalTo(week_num).once('value').then(function(snapshot) {
+	return database.ref("passwords").orderByKey().equalTo(week_num).once('value').then(function(snapshot) {
         snapshot = snapshot.child(week_num);
-		console.log("Snapshot returned");
-		if (snapshot.hasChild('week')) {
-			// week already exists, now check password
-            database.ref("passwords").orderByKey().equalTo(week_num).once('value').then(function(snapshot) {
+        if (snapshot.val() == document.getElementById('password').value) {
+            return database.ref("weeks").orderByKey().equalTo(week_num).once('value').then(function(snapshot) {
                 snapshot = snapshot.child(week_num);
-                if (snapshot.val() == document.getElementById('password').value) {
+                console.log("Snapshot returned");
+                if (snapshot.hasChild('week')) {
+                    // password and week both exist, upload just the puzzle
                     update = {};
-
+        
                     week_num = document.getElementById('create-week').value;
                     week = week_num < 10 ? "0" + week_num : "" + week_num;
-
+        
                     artistName = document.getElementById('create-author').value;
                     update["/title"] = document.getElementById('create-title').value;
                     update["/info"] = document.getElementById('create-info').value;
                     update["/image"] = document.getElementById('create-image').value;
                     database.ref('weeks/' + week + '/puzzles/' + artistName).update(update);
+                } else {
+                    // password exists but not week, so create full week object
+                    update = {};
+                    
+                    week_num = document.getElementById('create-week').value;
+                    week = week_num < 10 ? "0" + week_num : "" + week_num;
+                    
+                    artistName = document.getElementById('create-author').value;
+                    update["/puzzles/" + artistName + "/title"] = document.getElementById('create-title').value;
+                    update["/puzzles/" + artistName + "/info"] = document.getElementById('create-info').value;
+                    update["/puzzles/" + artistName + "/image"] = document.getElementById('create-image').value;
+                    update["/week"] = week_num;
+                    database.ref('weeks/' + week).update(update);
                 }
-            })
-		} else {
-			// week does not exist, do nothing
-		}
-	}).then(function() {
-		return false;
-	});
+            }).then(function() {
+                return false;
+            });
+        }
+    }).then(function() {
+        return false;
+    });
+    
+    
 }
