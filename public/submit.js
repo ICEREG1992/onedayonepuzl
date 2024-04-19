918208a6a24471c54359aa5e4ff99a688d4f152b
William Sullivan <willwill.sullivan@gmail.com>
2024-04-19 00:31:14 -0500

    remove password system

--- public/submit.js
+++ public/submit.js
@@ -16,45 +16,37 @@ var database = firebase.database();
 function postPuzzle(elem) {
 	week_num = document.getElementById('create-week').value;
     try {
-        return database.ref("passwords").orderByKey().equalTo(week_num).once('value').then(function(snapshot) {
+        snapshot = snapshot.child(week_num);
+        return database.ref("weeks").orderByKey().equalTo(week_num).once('value').then(function(snapshot) {
             snapshot = snapshot.child(week_num);
-            if (snapshot.val() == document.getElementById('password').value.toLowerCase()) {
-                return database.ref("weeks").orderByKey().equalTo(week_num).once('value').then(function(snapshot) {
-                    snapshot = snapshot.child(week_num);
-                    console.log("Snapshot returned");
-                    if (snapshot.hasChild('week')) {
-                        // password and week both exist, upload just the puzzle
-                        update = {};
-            
-                        week_num = document.getElementById('create-week').value;
-                        week = week_num < 10 ? "0" + week_num : "" + week_num;
-            
-                        artistName = document.getElementById('create-author').value;
-                        update["/title"] = document.getElementById('create-title').value;
-                        update["/info"] = document.getElementById('create-info').value;
-                        update["/image"] = document.getElementById('create-image').value;
-                        return database.ref('weeks/' + week + '/puzzles/' + artistName).update(update, onComplete);
-                    } else {
-                        // password exists but not week, so create full week object
-                        update = {};
-                        
-                        week_num = document.getElementById('create-week').value;
-                        week = week_num < 10 ? "0" + week_num : "" + week_num;
-                        
-                        artistName = document.getElementById('create-author').value;
-                        update["/puzzles/" + artistName + "/title"] = document.getElementById('create-title').value;
-                        update["/puzzles/" + artistName + "/info"] = document.getElementById('create-info').value;
-                        update["/puzzles/" + artistName + "/image"] = document.getElementById('create-image').value;
-                        update["/week"] = week_num;
-                        return database.ref('weeks/' + week).update(update, onComplete);
-                    }
-                }).then(res => {
-                    return false;
-                });
+            console.log("Snapshot returned");
+            if (snapshot.hasChild('week')) {
+                //  week exists, upload just the puzzle
+                update = {};
+    
+                week_num = document.getElementById('create-week').value;
+                week = week_num < 10 ? "0" + week_num : "" + week_num;
+    
+                artistName = document.getElementById('create-author').value;
+                update["/title"] = document.getElementById('create-title').value;
+                update["/info"] = document.getElementById('create-info').value;
+                update["/image"] = document.getElementById('create-image').value;
+                return database.ref('weeks/' + week + '/puzzles/' + artistName).update(update, onComplete);
             } else {
-                updateStatus("Invalid password!", "Red");
+                // week doesn't exist, so create full week object
+                update = {};
+                
+                week_num = document.getElementById('create-week').value;
+                week = week_num < 10 ? "0" + week_num : "" + week_num;
+                
+                artistName = document.getElementById('create-author').value;
+                update["/puzzles/" + artistName + "/title"] = document.getElementById('create-title').value;
+                update["/puzzles/" + artistName + "/info"] = document.getElementById('create-info').value;
+                update["/puzzles/" + artistName + "/image"] = document.getElementById('create-image').value;
+                update["/week"] = week_num;
+                return database.ref('weeks/' + week).update(update, onComplete);
             }
-        }).then(function() {
+        }).then(res => {
             return false;
         });
     } catch(e) {
